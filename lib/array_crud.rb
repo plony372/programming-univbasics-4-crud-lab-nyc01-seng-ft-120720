c9c8505dce76649685484371d6ca14b19a67152b
plony benav <menachemfuterfas@gmail.com>
2020-11-11 16:17:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,25 +15,25 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  new = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  new = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  new = array.index(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  new = array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  new = array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array.index[index_number] = element
 end
